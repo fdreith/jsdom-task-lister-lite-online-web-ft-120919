0b30b025ae4ce2228cc0e922dc46ee460a7e0145
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-28 13:40:05 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form")
+  form.addEventListener('submit', addTaskToList)
+
+  function addTaskToList(event) {
+    event.preventDefault()
+    const taskInput = document.getElementById("new-task-description")
+    const task = taskInput.value
+    const tasks = document.getElementById("tasks")
+    tasks.innerHTML += `<li> ${task} </li>`
+    taskInput.value = ""
+  }
+
 });
